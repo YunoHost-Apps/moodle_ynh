58e96aa627d59a17cc1b247e9bc5d8267a4d7389
anmol26s <github@datamol.org>
2018-09-01 18:02:40 +0530

    Fix for Yunohost 3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,58 +12,59 @@ ynh_delete_file_checksum () {
 	ynh_app_setting_delete $app $checksum_setting_name
 }
 
-ynh_install_php7 () {
-
-  ynh_package_update
-  ynh_package_install apt-transport-https --no-install-recommends
-
-  wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.list
-
-  ynh_package_update
-  ynh_install_app_dependencies php7.1 php7.1-zip php7.1-fpm php7.1-mysql php7.1-xml php7.1-intl php7.1-mbstring php7.1-gd php7.1-curl php7.1-soap php7.1-pgsql php7.1-xmlrpc postgresql-9.4
-  sudo update-alternatives --install /usr/bin/php php /usr/bin/php5 70
-}
-
-ynh_remove_php7 () {
-  sudo rm -f /etc/apt/sources.list.d/php7.list
-  sudo apt-key del 4096R/89DF5277
-  sudo apt-key del 2048R/11A06851
-  ynh_remove_app_dependencies php7.1 php7.1-zip php7.1-fpm php7.1-mysql php7.1-xml php7.1-intl php7.1-mbstring php7.1-gd php7.1-curl php7.1-soap php7.1-pgsql php7.1-xmlrpc postgresql-9.4
-}
-
-
-# Create a dedicated php-fpm config for php7.1
+# Send an email to inform the administrator
 #
-# usage: ynh_add_fpm_config
-ynh_add_php7.1-fpm_config () {
-	finalphpconf="/etc/php/7.1/fpm/pool.d/$app.conf"
-	ynh_backup_if_checksum_is_different "$finalphpconf"
-	sudo cp ../conf/php-fpm.conf "$finalphpconf"
-	ynh_replace_string "__NAMETOCHANGE__" "$app" "$finalphpconf"
-	ynh_replace_string "__FINALPATH__" "$final_path" "$finalphpconf"
-	ynh_replace_string "__USER__" "$app" "$finalphpconf"
-	sudo chown root: "$finalphpconf"
-	ynh_store_file_checksum "$finalphpconf"
+# usage: ynh_send_readme_to_admin app_message [recipients]
+# | arg: app_message - The message to send to the administrator.
+# | arg: recipients - The recipients of this email. Use spaces to separate multiples recipients. - default: root
+#	example: "root admin@domain"
+#	If you give the name of a YunoHost user, ynh_send_readme_to_admin will find its email adress for you
+#	example: "root admin@domain user1 user2"
+ynh_send_readme_to_admin() {
+	local app_message="${1:-...No specific information...}"
+	local recipients="${2:-root}"
 
-	if [ -e "../conf/php-fpm.ini" ]
+	# Retrieve the email of users
+	find_mails () {
+		local list_mails="$1"
+		local mail
+		local recipients=" "
+		# Read each mail in argument
+		for mail in $list_mails
+		do
+			# Keep root or a real email address as it is
+			if [ "$mail" = "root" ] || echo "$mail" | grep --quiet "@"
+			then
+				recipients="$recipients $mail"
+			else
+				# But replace an user name without a domain after by its email
+				if mail=$(ynh_user_get_info "$mail" "mail" 2> /dev/null)
+				then
+					recipients="$recipients $mail"
+				fi
+			fi
+		done
+		echo "$recipients"
+	}
+	recipients=$(find_mails "$recipients")
+
+	local mail_subject="☁️🆈🅽🅷☁️: \`$app\` has important message for you"
+
+	local mail_message="This is an automated message from your beloved YunoHost server.
+Specific information for the application $app.
+$app_message
+---
+Automatic diagnosis data from YunoHost
+$(yunohost tools diagnosis | grep -B 100 "services:" | sed '/services:/d')"
+	
+	# Define binary to use for mail command
+	if [ -e /usr/bin/bsd-mailx ]
 	then
-		finalphpini="/etc/php/7.1/fpm/conf.d/20-$app.ini"
-		ynh_backup_if_checksum_is_different "$finalphpini"
-		sudo cp ../conf/php-fpm.ini "$finalphpini"
-		sudo chown root: "$finalphpini"
-		ynh_store_file_checksum "$finalphpini"
+		local mail_bin=/usr/bin/bsd-mailx
+	else
+		local mail_bin=/usr/bin/mail.mailutils
 	fi
 
-	sudo systemctl reload php7.1-fpm
-}
-
-
-# Remove the dedicated php-fpm config for php7.1
-#
-# usage: ynh_remove_fpm_config
-ynh_remove_php7.1-fpm_config () {
-	ynh_secure_remove "/etc/php/7.1/fpm/pool.d/$app.conf"
-	ynh_secure_remove "/etc/php/7.1/fpm/conf.d/20-$app.ini" 2>&1
-	sudo systemctl reload php7.1-fpm
+	# Send the email to the recipients
+	echo "$mail_message" | $mail_bin -a "Content-Type: text/plain; charset=UTF-8" -s "$mail_subject" "$recipients"
 }
