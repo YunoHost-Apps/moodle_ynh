5030a917f443b2f0742cef45c7da721458825aa7
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-12-12 10:25:13 +0100

    Testing (#55)
    * 3.11.4

---
+++ conf/config-domain.php
@@ -0,0 +1,29 @@
+<?php
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = '__DB_NAME__';
+$CFG->dbuser    = '__DB_NAME__';
+$CFG->dbpass    = '__DB_PWD__';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array(
+    'dbpersist' => 0,
+    'dbsocket'  => '',
+    'dbport'    => '',
+);
+
+$CFG->wwwroot   = 'https://__DOMAIN__';
+$CFG->dataroot  = '__DATA_PATH__';
+$CFG->admin = 'admin';
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php'); // Do not edit
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
