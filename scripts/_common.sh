f83e74dc4fe7a8814525e98d9b74b7504a27b757
ericgaspar <junk.eg@free.fr>
2022-05-20 19:09:17 +0200

    PHP8

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,9 @@
 #=================================================
 
 # dependencies used by the app
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
-pkg_dependencies="postgresql php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
+pkg_dependencies="postgresql php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
 
 #=================================================
 # PERSONAL HELPERS
