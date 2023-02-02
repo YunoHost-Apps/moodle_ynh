ef2101e4b18496755b02b36640c33c113c39b81e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-02 15:54:46 +0100

    4.1.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
 # dependencies used by the app
-pkg_dependencies="postgresql php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
+php_dependencies="php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
+
+pkg_dependencies="postgresql $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
