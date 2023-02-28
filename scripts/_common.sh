bb2b5968a9f391ef7467851f205df2bf7422e68e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-28 20:37:00 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,11 +6,6 @@
 
 YNH_PHP_VERSION="8.0"
 
-# dependencies used by the app
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
-
-#REMOVEME? pkg_dependencies="postgresql $php_dependencies"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
