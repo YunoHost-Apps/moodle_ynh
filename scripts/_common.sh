f9b459ee47e42223cb6d2d0d41d8079d72cd6ce1
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-01 13:51:19 +0100

    Testing (#79)
    * Upgrade (#68)
    
    * Apply example_ynh
    
    * Fix melange-de-versions
    
    https://forum.yunohost.org/t/melange-de-versions-de-moodle-detectees/19985
    
    * Update manifest.json
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * 4.0.2 (#71)
    
    * 4.0.2
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Auto-update README
    
    * Bullseye (#73)
    
    * Update manifest.json
    
    * Auto-update README
    
    * Update upgrade
    
    * Fix
    
    * Auto-update README
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * 4.0.3 (#74)
    
    * 4.0.3
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * 4.0.4
    
    * Auto-update README
    
    * 4.1.0 (#77)
    
    * 4.1.0
    
    * Auto-update README
    
    * fix PHP
    
    * Update config_panel.toml
    
    * Update config_panel.toml
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Auto-update README
    
    * 4.1.1
    
    * Auto-update README
    
    * Update extra_php-fpm.conf
    
    * Update extra_php-fpm.conf
    
    * Auto-update README
    
    * Update manifest.json
    
    * Update extra_php-fpm.conf
    
    * Update _common.sh
    
    ---------
    
    Co-authored-by: yalh76 <yalh@yahoo.com>
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,9 @@
 YNH_PHP_VERSION="7.4"
 
 # dependencies used by the app
-pkg_dependencies="postgresql php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
+php_dependencies="php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-ldap"
+
+pkg_dependencies="postgresql $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
